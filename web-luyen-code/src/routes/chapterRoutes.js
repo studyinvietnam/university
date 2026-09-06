5db5613c8b445f0712cbcf67bd7818fcd36c3369
Hùng Anh:Admin-PC <hunganh.okokok.0@gmail.com>
2026-09-06 10:58:53 +0700

    Remove web-luyen-code:)))) 10-58-53_06-09-2026

--- web-luyen-code/src/routes/chapterRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const chapterController = require('../controllers/chapterController');
-const authMiddleware = require('../middlewares/authMiddleware');
-
-// Lấy danh sách bài tập của một chương
-router.get('/:id/problems', authMiddleware, chapterController.getProblemsByChapter);
-
-module.exports = router;
