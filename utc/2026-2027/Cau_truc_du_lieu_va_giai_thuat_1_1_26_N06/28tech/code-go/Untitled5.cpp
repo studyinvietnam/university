acd080614bd8b2d2fe0a6685a19752de7b50ffb3
Hùng Anh:Admin-PC <hunganh.okokok.0@gmail.com>
2026-09-11 21:33:31 +0700

    Update 21-33-30_11-09-2026

---
+++ utc/2026-2027/Cau_truc_du_lieu_va_giai_thuat_1_1_26_N06/28tech/code-go/Untitled5.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int chon;
+    cout << "Vui long chon case: " << flush;
+    cin >> chon;
+    cout << endl;
+    switch(chon){
+    	case 1:{
+		/*== [Comparator Lower_bound Upper_bound]. Bài 3. Lower_bound, Upper_bound ==*/
+		
+		}
+	}
+	return 0;
+}
